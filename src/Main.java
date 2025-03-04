b912d330542513a783913c0c97bf816960e64dbc
LevMarkos <levmarcosyan@mail.ru>
2025-03-04 18:46:03 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -39,7 +39,6 @@ public class Main {
     public static int countOccurrences(String str, String subStr) {
         int count = 0;
         int index = 0;
-
         while ((index = str.indexOf(subStr, index)) != -1) {
             count++;
             index += subStr.length();
